070ce368f4d19cce36862cf2313163af48741148
Bennett Lee <bennettl908@yahoo.com>
2014-03-27 03:14:05 -0700

    Fix safari bug dealing with iFrames

--- tapiture-safari.safariextension/tapiture_injected.js
+++ tapiture-safari.safariextension/tapiture_injected.js
@@ -1,18 +1,21 @@
 // Event Listeners
-document.addEventListener("mousedown", rightClickCallBack, false);
-safari.self.addEventListener("message", contextMenuItemCallBack, false); // Message comes from global.html when context menu item is clicked
+// if (window.top === window) {
+    console.log('hey');
+    document.addEventListener("mousedown", rightClickCallBack, false);
+    safari.self.addEventListener("message", contextMenuItemCallBack, false); // Message comes from global.html when context menu item is clicked
+    console.log('hey im being injected');
 
-console.log('hey');
-console.log('hey', document.title);
-console.log('wgwegwe', $("body"));
+// }
+
 // Global variable
 var userInfo = {};
 
 // Set up information for userInfo global variable
 function rightClickCallBack(event) {
+
     if (event.which === 3 || event.button === 2) {
         var clickOnImage = (event.target.nodeName == "IMG") ? true : false;
-
+        console.log('right click deteched');
         // if he did, set an image src
         var imageSrc = (clickOnImage) ? event.target.src : null;
 
@@ -23,7 +26,7 @@ function rightClickCallBack(event) {
 
         console.log("user right clicked " + userInfo);
         // Send userInfo to global.html
-        safari.self.tab.setContextMenuEventUserInfo(event, userInfo);
+        safari.self.tab.setContextMenuEventUserInfo(event, 'userInfo');
     }
 }
 
@@ -31,10 +34,18 @@ function rightClickCallBack(event) {
 // If the user clicked on a single image, call tap, else call addOverlay();
 function contextMenuItemCallBack(event) {
     if (event.name == "userClickTapThis") {
-        if (userInfo.clickOnImage) {
+
+        if (userInfo.clickOnImage && userInfo.srcUrl) {
             tap(userInfo);
-        } else {
-            addOverlay();
+            // For iframes, userInfo will still contain srcUrl
+            // Since every iframe is listening to this event, multiple taps can pop up
+            userInfo = [];
+        } else if (!userInfo.clickOnImage){
+            // E every iframe is listening to this event. Only add overlay to the main document, not in iframes
+            if (window.top === window) {
+                addOverlay();
+            }
         }
     }
 }
+
